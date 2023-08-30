852716d83df60b8027a311ffcb2a031e8419eb0a
AlanSt11 <86734556+AlanSt11@users.noreply.github.com>
2023-08-30 13:07:47 -0600

    Add files via upload

---
+++ view/V_form.php
@@ -0,0 +1,39 @@
+<div class="post-form">
+    <div class="form width-height" id="div-form">
+        <h2 class="title">Formulario de contacto</h2>
+        <form action="" class="contact-form" method="POST" id="form-info">
+            <div class="content-form">
+                <input type="text" class="form-input" id="name" name="name" placeholder=" ">
+                <label for="name" class="form-label">Nombre:</label>
+            </div>
+
+            <div class="space"></div>
+
+            <div class="content-form">
+                <input type="email" class="form-input" id="email" name="email" placeholder=" ">
+                <label for="email" class="form-label">Correo:</label>
+            </div>
+
+            <div class="space"></div>
+
+            <div class="content-form">
+                <textarea name="message" id="message" cols="30" rows="10" placeholder=" "class="form-input"></textarea>
+                <label for="message" class="form-label">Mensaje:</label>
+            </div>
+
+            <button class="btn-submit" name="send">
+                <div class="svg-wrapper-1">
+                    <div class="svg-wrapper">
+                        <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0 0h24v24H0z" fill="none"></path>
+                            <path
+                                d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+                                fill="currentColor"></path>
+                        </svg>
+                    </div>
+                </div>
+                <span>Enviar</span>
+            </button>
+        </form>
+    </div>
+</div>
